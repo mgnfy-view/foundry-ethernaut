1e24b073abed68e8e9af5c25d9e258c5451f9b0d
Sahil-Gujrati <sahilgujrati12@gmail.com>
2024-03-09 18:34:17 +0530

    Fix formatting issue

--- script/08-vault/deployVault.s.sol
+++ script/08-vault/deployVault.s.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {Script} from "forge-std/Script.sol";
-import {Vault} from "@src/08-Vault/Vault.sol";
-
+import { Script } from "forge-std/Script.sol";
+import { Vault } from "@src/08-Vault/Vault.sol";
 
 contract DeployVault is Script {
     function run() external {
-        vm.startBroadcast(uint256(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80)); // use one of the default private keys that comes with anvil
+        // use one of the default private keys that comes with anvil
+        vm.startBroadcast(uint256(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80));
         Vault vault = new Vault(bytes32("Secret!"));
         vm.stopBroadcast();
     }
